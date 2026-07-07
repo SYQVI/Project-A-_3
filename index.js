1143b6eb01343d1749874c5a2447b7cc3954a8b4
SYQVI <waeililnzy@gmail.com>
2026-07-07 22:34:36 +0300

    Update config file reference from config.json to config.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, GatewayIntentBits } = require("discord.js");
-const config = require("./config.json");
+const config = require('./config.js');
 const fs = require("fs");
 const path = require("path"); 
 
